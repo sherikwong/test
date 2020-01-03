5d151c3fe40a0b4c0e9b2d94509868c47f2bf39e
Sheri Kwong <sherikwong@gmail.com>
2020-01-03 11:04:47 -0500

    Finished

--- src/app/error-subscription/error-subscription.component.ts
+++ src/app/error-subscription/error-subscription.component.ts
@@ -5,21 +5,24 @@ import { startWith, map, tap, withLatestFrom, filter, scan, switchMap, mergeMap,
 import { binarySearchArrays } from './util';
 import { ChangeDetectorStatus } from '@angular/core/src/change_detection/constants';
 
-export type IFormValidatorErrorFn = (name: any, control: FormControl) => string
+export type FormControlErrorMessageFn = ((params: FormControlErrorMessageParams) => string);
 
-export type FormControlErrorMessageFn = ((params: FormControlErrorMessages) => any);
-type FormControlErrorsMap = Map<string, string>;
-
-export interface FormControlErrorMessages {
-    [controlName: string]: { [errorName: string]: string }
+export interface FormControlErrorMessageEval {
+  [errorName: string]: string | FormControlErrorMessageFn;
 }
 
-interface FormControlErrorMessage {
-    controlName: string;
-    errorName: string;
-    control: AbstractControl;
-    value: any;
-    errorDetails: {[key: string]: any};
+export type FormControlErrorMessages = {
+  [controlName: string]: {
+    [errorName: string]: string;
+  }
+}
+
+interface FormControlErrorMessageParams {
+  controlName: string;
+  errorName: string;
+  control: AbstractControl;
+  value: any;
+  errorDetails: { [key: string]: any };
 }
 
 @Component({
@@ -27,15 +30,9 @@ interface FormControlErrorMessage {
   templateUrl: './error-subscription.component.html'
 })
 export class ErrorSubscriptionComponent implements OnInit {
-  formControlsErrors = new Map<string, ControlErrorsMap>();
-  oldFormGroup: any;
-  possibleErrorMessages: { [key: string]: ErrorMessage };
-  errorObservMap = {};
   formGroup: FormGroup;
   formErrors: Subject<any> = new Subject();
-  counter = 0;
-  subscription;
-  blah;
+  errorMessages;
 
   constructor(private fb: FormBuilder) { }
 
@@ -45,164 +42,65 @@ export class ErrorSubscriptionComponent implements OnInit {
       control2: [null, Validators.compose([this.validator, Validators.required])]
     });
 
-    this.possibleErrorMessages = {
-      required: () => `This field is required`,
-      Custom: 'Minimum length blah blah',
-      minLength: 'lasfjasl',
-      la: 'asofiahjsfoij'
-    };
+    const errorEvaluations: FormControlErrorMessageEval = {
+      required: ({ controlName }) => `The ${controlName} field is required`,
+      'Custom': ({controlName, value}) => `This control(${controlName})'s ${value} is custom`
 
-    this.subscription = this.errorMessages(this.formGroup).subscribe(value => { console.log('Final result', value); console.log('======================='); this.blah = value; });
+    };
+    
+    this.formGroup.valueChanges.subscribe(value => {
+      this.errorMessages = this.getErrorMessages(this.formGroup, errorEvaluations);
+      console.log(this.errorMessages);
+    });
   }
 
   restart() {
-    this.subscription.unsubscribe();
     this.ngOnInit();
   }
-
-  getErrorMessages(formGroup: FormGroup, allErrorFnObj: {[errorName: string]: string | FormControlErrorMessageFn} ) {    
-    return Object.entries(formGroup.controls).reduce((accumMessages, [controlName, control]) => {
+  
+  /**
+   * Created to be used within a value/status form group subscription. Given a Map<errorName string, error string | (error params) => string>, output updated error messages
+   * 
+   * **Note:** This will not trigger on values initiated w/ form group at the same time. You must initate FormGroup, subscribe, and then update/patch value.
+   * 
+   * **Reminder:** DO NOT FORGET TO UNSUBSCRIBE TO CHANGES ON DESTROY!
+   * @param formGroup 
+   * @param allErrorFnObj 
+   * 
+   * 
+   * @example ```javascript
+   * this.formGroup.valueChanges.subscribe(value => {
+   *   this.getErrorMessages(this.formGroup, {
+   *     required: ({ controlName }) => `The ${controlName} field is required`,
+   *   }) <--- Do something with this return
+   * });
+   * ```
+   */
+  getErrorMessages(formGroup: FormGroup, allErrorFnObj: { [errorName: string]: string | FormControlErrorMessageFn }) {
+    const allMessagesObj: FormControlErrorMessages = Object.entries(formGroup.controls).reduce((allMessages, [controlName, control]) => {
       const thisControlErrors = formGroup.get(controlName).errors;
       if (thisControlErrors) {
-        
-        const reducedEvaluatedErrors: Map<string, string> = Object.entries(thisControlErrors).reduce((thisControlErrorMessages, [errorName, errorDetails]) => {
-          const errorMessageParams: FormControlErrorMessage = {
+
+        const reducedEvaluatedErrors: {[errorName: string]: string} = Object.entries(thisControlErrors).reduce((thisControlErrorMessages, [errorName, errorDetails]) => {
+          const errorMessageParams: FormControlErrorMessageParams = {
             controlName,
             control,
             value: control.value,
             errorName,
             errorDetails
           };
-          
-          const evaluatedMessage = typeof allErrorFnObj[controlName] === 'function' ? (allErrorFnObj[controlName] as FormControlErrorMessageFn)(errorMessageParams) : allErrorFnObj[controlName];
-          
-          thisControlErrorMessages.set(errorName, allErrorFnObj[controlName](errorMessageParams));
+
+          const evaluatedMessage = typeof allErrorFnObj[errorName] === 'function' ? (allErrorFnObj[errorName] as FormControlErrorMessageFn)(errorMessageParams) : allErrorFnObj[errorName];
+          thisControlErrorMessages[errorName] = evaluatedMessage;
           return thisControlErrorMessages;
-        }, new Map());
-        
-        accumMessages.set(controlName, reducedEvaluatedErrors);
-        return accumMessages;
+        }, {});
+
+        allMessages[controlName] = reducedEvaluatedErrors;
+        return allMessages;
       }
-    }, new Map<string, Map<string, string>>());
-  }
-  
-
-  errorMessages(formGroup) {
-    if (!this.oldFormGroup) this.oldFormGroup = formGroup.value;
-
-    let previousObserv;
-
-    const createObservs = () => combineLatest(...Object.entries(formGroup.controls).map(([controlName, control]) => this.createErrorMessageObservable(controlName, control as AbstractControl)));
-
-    return createObservs().pipe(
-      withLatestFrom(this.formGroupControlsChanged(formGroup))
-    );
-
-    // return combineAll(this.formGroupControlsChanged(formGroup), createObservs()).pipe(
-      // map(response => {console.log(response); return response;}),
-      // switchMap((value) => {
-        // console.log(value);
-        // return of(['', '']);
-      // })
-      // switchMap((response: any) => {
-        // console.log(response);
-        // if (changes && (changes.added || changes.deleted)) return createObservs();
-        // return of(errors);
-        // return of('')
-      // }),
-      // map(errors => this.massageOutput(errors))
-    // )
-  }
-
-
-  private formGroupControlsChanged(formGroup: FormGroup): Observable<FormGroupChanges> {
-    const oldFormControlNames = Object.keys(this.oldFormGroup);
-
-    return formGroup.valueChanges.pipe(
-      startWith({}),
-      map(newFormGroup => {
-        const newFormControlNames = Object.keys(newFormGroup);
-
-        const changedControls = {
-          deleted: null,
-          added: null
-        } as FormGroupChanges;
-
-        if (oldFormControlNames.length !== newFormControlNames.length) { // If the formGroups are different
-          if (newFormControlNames.length) {
-            if (oldFormControlNames.length < newFormControlNames.length) {
-              changedControls.added = binarySearchArrays(oldFormControlNames, newFormControlNames)[0];
-            } else if (oldFormControlNames.length > newFormControlNames.length) {
-              changedControls.deleted = binarySearchArrays(oldFormControlNames, newFormControlNames, false)[0];
-            }
-            this.oldFormGroup = newFormGroup;
-          }
-          return changedControls;
-        }
-        return null;
-      })
-    )
-  }
-
-  private massageOutput(errors): any {
-    return errors.reduce((accum, allControlErrors: ErrorDetails[]) => {
-      if (allControlErrors && allControlErrors.length) {
-        const currentControlErrorMessages = allControlErrors.reduce((dummyAccum, { controlName, errorMessage }: ErrorDetails) => {
-          dummyAccum[controlName] = dummyAccum[controlName] ? [...dummyAccum[controlName], errorMessage] : [errorMessage];
-          return dummyAccum;
-        }, {} as any);
-        return { ...accum, ...currentControlErrorMessages };
-      }
-      return accum;
     }, {});
+    return allMessagesObj;
   }
-
-  private labelControlErrors(errors: any[]) {
-    return errors.reduce((dummyAccum, { controlName, errorMessage }: ErrorDetails) => {
-      dummyAccum[controlName] = dummyAccum[controlName] ? [...dummyAccum[controlName], errorMessage] : [errorMessage];
-      return dummyAccum;
-    }, {} as any);
-  }
-
-  private createErrorMessageObservable(controlName: string, control: AbstractControl): Observable<any[]> {
-    return control && control.statusChanges.pipe( // Pipe each status change observable from form controls
-      startWith(control.valid ? 'VALID' : 'INVALID'), // Feign first response otherwise first touch change will not be registered
-      map(() => {
-        if (!control.valid) {
-          this.formControlsErrors.set(controlName, new Map());
-
-          Object.entries(control.errors).forEach(([errorName, errorDetails]) => { // Loop through current control's errors
-            let errorMessage = '';
-            const associatedErrorValue = this.possibleErrorMessages[errorName as string];
-
-            const evaluateMessage = message => typeof associatedErrorValue === 'function' ? message(controlName, control, errorName, errorDetails) : message; // Evaluate error's value from possibleErrorMessages param
-
-            if (associatedErrorValue) { // If the current control's error evaluation is exists incoming errorMessages
-
-              errorMessage = evaluateMessage(associatedErrorValue); // If fn, evaluated. Otherwise, use string
-              this.formControlsErrors.get(controlName).set(errorName, errorMessage);
-
-            } else { // If error is not defined, use default or passed in error message
-
-              const undefinedErrorMessage = (control) => `There was an error with the field ${controlName} with value ${control.value}`;
-              this.formControlsErrors.get(controlName).set(undefined, undefinedErrorMessage(control));
-
-            }
-          })
-        } else {
-          if (!this.formControlsErrors.has(controlName)) this.formControlsErrors.delete(controlName); // If control is valid, delete existing map entry from
-        }
-
-        const controlErrors = Array.from(this.formControlsErrors.get(controlName)).map(([errorName, error]) => { // Get the errors for this control
-          return { controlName, value: control.value }; // For each control error, format like so
-          // return { controlName, errorMessage: error } as ErrorDetails; // For each control error, format like so
-        });
-        return controlErrors;
-      })
-    )
-  }
-
-
 
   validator(): ValidationErrors {
     return {
@@ -220,11 +118,6 @@ export class ErrorSubscriptionComponent implements OnInit {
         invalid: true
       }
     };
-  }
-
-  check() {
-    console.log(this.formGroup.controls);
-    console.log(this.blah);
   }
 
   addControl() {
